0a36559051519b6d70c48015d78fe200a85e8dd5
zgotter <shkim4738@gmail.com>
2023-03-25 21:08:30 +0900

    step02 Function

---
+++ steps/step02.py
@@ -0,0 +1,31 @@
+from step01 import Variable
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        return x**2
+
+
+def main():
+    import numpy as np
+
+    x = Variable(np.array(10))
+    f = Square()
+    y = f(x)
+    print(type(y))
+    print(y.data)
+
+
+if __name__ == "__main__":
+    main()
