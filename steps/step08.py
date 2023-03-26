1e01a63067579cccd8f4ef2a0ecf6095eb827010
zgotter <shkim4738@gmail.com>
2023-03-26 23:13:19 +0900

    step07 Backpropagation automation with loop

---
+++ steps/step08.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        output.set_creator(self)  # 출력 변수에 창조자를 설정
+        self.input = input
+        self.output = output  # 출력도 저장
+        return output
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x**2
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+
+def main():
+    A = Square()
+    B = Exp()
+    C = Square()
+
+    x = Variable(np.array(0.5))
+    a = A(x)
+    b = B(a)
+    y = C(b)
+
+    # 역전파
+    y.grad = np.array(1.0)
+    y.backward()
+    print(x.grad)
+
+
+if __name__ == "__main__":
+    main()
